56583de1f5b4aa8f87b83ec55c2b2c2ca241def2
kmunene <kmunene529@gmail.com>
2023-11-22 16:07:14 +0300

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
+
+# AUTHOR - kelvin munene
+
+'''A Square class'''
+
+
 class Square:
+    '''Represents a Square'''
+
+    '''constructor for square class'''
     def __init__(self, size=0):
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
     @property
     def size(self):
-        return self.__size
+        '''getter function for size'''
+        return (self.__size)
 
     @size.setter
     def size(self, value):
+        '''setter function for size'''
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+            raise TypeError('size must be an integer')
+        if (self.__size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
-        return self.__size ** 2
+        res = self.__size ** 2
+        return res

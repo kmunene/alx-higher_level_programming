707dc9aed33fe593384e4077dd9f1b74000cb993
kmunene <kmunene529@gmail.com>
2023-11-22 15:59:02 +0300

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+
+
+# Author: kelvin
+
+
 class Square:
+    '''innitialize the class'''
     def __init__(self, size=0):
         self.__size = size
-        if(type(size) is not int):
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif(size < 0):
+        elif (size < 0):
             raise ValueError("size must be >= 0")
+
     def area(self):
+        '''returns area'''
         s = self.__size * self.__size
         return s

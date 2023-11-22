01f305cca8ae5cc57fd270c8e65086aa1e1878d3
kmunene <kmunene529@gmail.com>
2023-11-22 15:50:38 +0300

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-
+# author: kelvin
 '''A Square class'''
+
 
 class Square:
     '''This is a class with a private square attribute'''
     def __init__(self, size):
+        '''private class attribute'''
         self.__size = size

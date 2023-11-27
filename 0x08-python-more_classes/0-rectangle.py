60091b79f9a4416e6dcccfe978f98072e6e41ec5
kmunene <kmunene529@gmail.com>
2023-11-27 19:40:53 +0300

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Rectangle Class
+
+This module defines a simple Rectangle class.
+
+Attributes:
+    No attributes are defined in this class.
+
+Methods:
+    No methods are defined in this class.
+"""
+
+
+class Rectangle:
+    """
+    Rectangle Class
+
+    A simple class representing a rectangle.
+
+    Methods:
+        No methods are defined in this class.
+    """
+    pass

0ad8ff86631f1aef60432fd5b260c32d4f18a8de
kmunene <kmunene529@gmail.com>
2023-11-27 21:15:42 +0300

    all files

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,116 +1,58 @@
 #!/usr/bin/python3
+
+# AUTHOR - kelvin munene
+
+'''represents a Rectangle class'''
+
+
 class Rectangle:
-    """
-    Rectangle Class
-
-    This class represents a rectangle with width and height attributes.
-
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-
-    Author: [Your Name]
-    Date: [Date]
-
-    """
-
+    '''defines a rectangle'''
     def __init__(self, width=0, height=0):
-        """
-        Constructor for the Rectangle class.
-
-        Args:
-            width (int): The width of the rectangle (default is 0).
-            height (int): The height of the rectangle (default is 0).
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than 0.
-
-        """
-        self.width = width
-        self.height = height
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = height
 
     @property
     def width(self):
-        """
-        Get the width of the rectangle.
-
-        Returns:
-            int: The width of the rectangle.
-
-        """
+        '''getter function: property'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Set the width of the rectangle.
-
-        Args:
-            value (int): The width to set.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-
-        """
+        '''setter function'''
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
     def height(self):
-        """
-        Get the height of the rectangle.
-
-        Returns:
-            int: The height of the rectangle.
-
-        """
+        '''getter function: property'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Set the height of the rectangle.
-
-        Args:
-            value (int): The height to set.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-
-        """
+        '''setter function'''
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
-        """
-        Calculate the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-
-        """
-        return self.__width * self.__height
+        '''defines area of rectangle'''
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        """
-        Calculate the perimeter of the rectangle.
-
-        Returns:
-            int: The perimeter of the rectangle or 0 if either width or height is zero.
-
-        """
-        res = 2 * (self.__width + self.__height)
-
-        if (self.__width == 0 or self.__height == 0):
+        '''defines perimeter of the rectangle'''
+        if self.__height == 0 or self.__width == 0:
             return 0
-        else:
-            return res
+        return (2 * (self.__height + self.__width))

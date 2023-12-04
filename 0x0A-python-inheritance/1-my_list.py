c659b1537ae3e5794f1a5209f8ba5a7c9849667a
kmunene <kmunene529@gmail.com>
2023-12-04 20:37:31 +0300

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+
+# Author - kelvin munene
+
+class MyList(list):
+    '''class thet takes in a list'''
+    def print_sorted(self):
+        '''prints the sorted list'''
+        print(sorted(self))

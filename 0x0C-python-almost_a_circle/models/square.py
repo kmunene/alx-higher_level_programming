6376b6f29b58bb05929e3f4d1a1a2211bdac0989
kmunene <kmunene529@gmail.com>
2023-12-11 20:49:58 +0300

    all

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,92 +1,83 @@
 #!/usr/bin/python3
-
+"""Defines a square class."""
 from models.rectangle import Rectangle
-from models.base import Base
 
 
 class Square(Rectangle):
-    """
-    Class representing a square, derived from the Rectangle class.
-    """
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square instance.
+        """Initialize a new Square.
 
-        Parameters:
-        - size (int): Size of the square.
-        - x (int): X-coordinate of the top-left corner of the square.
-        - y (int): Y-coordinate of the top-left corner of the square.
-        - id (int): Optional. If provided, sets the id of the instance;
-
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
-        self.size = size
-
-    def __str__(self):
-        """
-        Return a string representation of the square.
-
-        Returns:
-        str: String representation of the square.
-
-        """
-        return f"[Square] ({self.id}) {self.__x}/{self.__y} - {self.__width}"
 
     @property
     def size(self):
-        """
-        Get the size of the square.
-
-        Returns:
-        int: Size of the square.
-
-        """
-        return self.__width
+        """Get/set the size of the Square."""
+        return self.width
 
     @size.setter
     def size(self, value):
-        """
-        Set the size of the square.
-
-        Parameters:
-        - value (int): Size value.
-
-        Raises:
-        - TypeError: If the provided value is not an integer.
-        - ValueError: If the provided value is less than or equal to 0.
-
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value <= 0:
-            raise ValueError("size must be > 0")
-        else:
-            self.__width = value
-            self.__height = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """
-        Update the attributes of the square.
+        """Update the Square.
 
-        Parameters:
-        - args (tuple): Positional arguments.
-        - kwargs (dict): Keyword arguments.
-
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
         """
-        # Implementation...
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-        """
-        Returns the dictionary representation of the Square.
-
-        Returns:
-        dict: Dictionary representation of the square.
-
-        """
+        """Return the dictionary representation of the Square."""
         return {
-            'id': self.id,
-            'x': self.__x,
-            'size': self.size,
-            'y': self.__y
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
         }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
